e13f3ef30661877f6b01f446e45a6d55f5027b65
axeMaltesse <l.wrzolek@wp.pl>
2017-02-20 13:49:42 +0000

    Changing the color of aim dot

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -30,7 +30,8 @@ public:
 		void Fire();
 
 protected:
-	UTankAimingComponent* TankAimingComponent = nullptr;
+	UPROPERTY(BlueprintReadOnly)
+		UTankAimingComponent* TankAimingComponent = nullptr;
 	UPROPERTY(BlueprintReadOnly)
 		UTankMovementComponent* TankMovementComponent = nullptr;
 
